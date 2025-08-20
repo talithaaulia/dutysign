c13cf34bbe34a67301c7da774487c6730a58bee9
najmma <najmahmakassar@gmail.com>
2025-08-20 12:40:25 +0700

    update aksi lihat pada fitur lihat laporan

--- resources/views/admin/detailReport.blade.php
+++ resources/views/admin/detailReport.blade.php
@@ -18,13 +18,46 @@
 
             <h5>Checklist Dokumen:</h5>
             <ul>
-                <li>{{ $report->foto_kegiatan ? '✔ Foto Kegiatan' : '✘ Foto Kegiatan' }}</li>
-                <li>{{ $report->scan_hardcopy ? '✔ Scan Hardcopy' : '✘ Scan Hardcopy' }}</li>
-                <li>{{ $report->e_toll ? '✔ E-Toll' : '✘ E-Toll' }}</li>
-                <li>{{ $report->bbm ? '✔ BBM' : '✘ BBM' }}</li>
-            </ul>
+                <li>
+                    {{ !empty($report->foto_kegiatan) ? '✔ Foto Kegiatan' : '✘ Foto Kegiatan' }}
+                    @if(!empty($report->foto_kegiatan))
+                        <div class="ms-4">
+                            <a href="{{ asset('storage/'.$report->foto_kegiatan) }}" target="_blank" class="btn btn-sm btn-info">Lihat</a> |
+                            <a href="{{ asset('storage/'.$report->foto_kegiatan) }}" download class="btn btn-sm btn-success">Download</a>
+                        </div>
+                    @endif
+                </li>
 
-            <hr>
+                <li>
+                    {{ !empty($report->scan_hardcopy) ? '✔ Scan Hardcopy' : '✘ Scan Hardcopy' }}
+                    @if(!empty($report->scan_hardcopy))
+                        <div class="ms-4">
+                            <a href="{{ asset('storage/'.$report->scan_hardcopy) }}" target="_blank" class="btn btn-sm btn-info">Lihat</a> |
+                            <a href="{{ asset('storage/'.$report->scan_hardcopy) }}" download class="btn btn-sm btn-success">Download</a>
+                        </div>
+                    @endif
+                </li>
+
+                <li>
+                    {{ !empty($report->e_toll) ? '✔ E-Toll' : '✘ E-Toll' }}
+                    @if(!empty($report->e_toll))
+                        <div class="ms-4">
+                            <a href="{{ asset('storage/'.$report->e_toll) }}" target="_blank" class="btn btn-sm btn-info">Lihat</a> |
+                            <a href="{{ asset('storage/'.$report->e_toll) }}" download class="btn btn-sm btn-success">Download</a>
+                        </div>
+                    @endif
+                </li>
+
+                <li>
+                    {{ !empty($report->bbm) ? '✔ BBM' : '✘ BBM' }}
+                    @if(!empty($report->bbm))
+                        <div class="ms-4">
+                            <a href="{{ asset('storage/'.$report->bbm) }}" target="_blank" class="btn btn-sm btn-info">Lihat</a> |
+                            <a href="{{ asset('storage/'.$report->bbm) }}" download class="btn btn-sm btn-success">Download</a>
+                        </div>
+                    @endif
+                </li>
+            </ul>
 
             <a href="{{ route('report.index') }}" class="btn btn-secondary">Kembali</a>
         </div>
